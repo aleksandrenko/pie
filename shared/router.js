a51032634bde149f2aa81a430c3538074bde1875
Deyan Yanakiev <kliomba@abv.bg>
2015-05-16 19:53:16 +0300

    Add product per id route

--- shared/router.js
+++ shared/router.js
@@ -5,3 +5,11 @@ Router.configure({
 Router.route('/', 'landingPage');
 
 Router.route('/products', 'productsList');
+
+Router.route('/product/:id', function () {
+  this.render('product', {
+    data: function () {
+      return Products.findOne({ _id: this.params.id });
+    }
+  });
+});
