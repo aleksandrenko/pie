0e9fcd79eda650ed0ee583a93bacdf4a79ffd38e
Deyan Yanakiev <kliomba@abv.bg>
2015-05-16 12:17:34 +0300

    Add mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.pie',
+  name: 'PIE',
+  description: 'Know what you buy. Buy what is known.',
+  author: 'PIE Team',
+  email: 'contact@pie.com',
+  website: 'http://pie.com'
+});
